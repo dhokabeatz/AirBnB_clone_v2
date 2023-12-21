dcf8066a073850005ddf75d75486b836e2995562
dhokabeatz <amglna2020@gmail.com>
2023-12-20 19:56:30 -0500

    All modified and delete method added for file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,8 +9,13 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    # def all(self):
+    #     """Returns a dictionary of models currently in storage"""
+    #     return FileStorage.__objects
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
         return FileStorage.__objects
 
     def new(self, obj):
@@ -53,3 +58,10 @@ class FileStorage:
                     self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()

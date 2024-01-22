6ae110b173d69ecbff2a5e2559e291e5bf6e8294
dhokabeatz <amglna2020@gmail.com>
2024-01-21 21:13:25 -0500

    web static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,14 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
 # Create a fake HTML file
-echo "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current

4b56d0c44e77b9e87a75408ba47e6fb4bafed65d
dhokabeatz <amglna2020@gmail.com>
2024-09-03 09:36:02 -0400

    script view name reviewed and corrected

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-
+"""
+This script starts a Flask web application:
+- The web application listens on 0.0.0.0, port 5000
+- Routes:
+    /: display “Hello HBNB!”
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":

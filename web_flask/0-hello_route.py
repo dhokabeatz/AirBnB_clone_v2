8b6557f738a283fba2d14e22ff2323fcb6610c5a
dhokabeatz <amglna2020@gmail.com>
2024-09-13 14:30:06 +0000

    App0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def home():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
